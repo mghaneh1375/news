ab01b3d23cf928531b95a48647a64646e943cf03
sina-ij <sinaijadi8888@yahoo.com>
2024-01-06 12:29:20 +0330

    idk

--- resources/views/user/newsShow.blade.php
+++ resources/views/user/newsShow.blade.php
@@ -8,8 +8,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
     <meta name="theme-color" content="#30b4a6" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/mainLogofav-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/mainLogofav-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#d2ac68">
     <meta name="msapplication-TileColor" content=" #e7e0d8">
@@ -63,9 +63,9 @@
         .eddsSec {}
 
         /* .eddsSec.fixedL {
-                                                                                                                                                                                                    position: fixed;
-                                                                                                                                                                                                    bottom: 0px;
-                                                                                                                                                                                                } */
+                                                                                                                                                                                                            position: fixed;
+                                                                                                                                                                                                            bottom: 0px;
+                                                                                                                                                                                                        } */
 
         .newsVideo {}
 
